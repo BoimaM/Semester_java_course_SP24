bb96258c28231ab49b8b6cfecd952f54cdba2481
BoimaM <MasfortBoima@gmail.com>
2024-02-22 20:35:47 -0500

    Update(s): Chapter 9 classes
    Signed-off-by: BoimaM <MasfortBoima@gmail.com>

--- src/chapter9_inheritance/TasteTester.java
+++ src/chapter9_inheritance/TasteTester.java
@@ -4,10 +4,7 @@ public class TasteTester {
 
     public static void main(String [] args){
 
-        Cake cake = new Cake("Vanilla",89.3 );
-        System.out.println("Cake 1 flavor: "+ cake.getFlavor());
-        System.out.println("Cake 1 price: "+ cake.getPrice());
-
+        //With default constructor
         BirthdayCake cake2 = new BirthdayCake();
         cake2.setFlavor("Peach");
         System.out.println("Cake 2 Flavor: " + cake2.getFlavor());
@@ -15,5 +12,10 @@ public class TasteTester {
         WeddingCake cake3 = new WeddingCake();
         cake3.setPrice(450.0);
         System.out.println("Cake 3 Price: " + cake3.getPrice());
+
+        //With constructor 2
+        Cake cake = new Cake("Vanilla",89.3 );
+        System.out.println("Cake 1 flavor: "+ cake.getFlavor());
+        System.out.println("Cake 1 price: "+ cake.getPrice());
     }
 }
