d60b155ffa0881951fef78b28f847e6296135143
BoimaM <MasfortBoima@gmail.com>
2024-03-06 00:31:33 -0500

    Update(s): Chapter 10 Package and Classes
    Signed-off-by: BoimaM <MasfortBoima@gmail.com>

---
+++ src/chapter10_polymorphism/Cat.java
@@ -0,0 +1,13 @@
+package chapter10_polymorphism;
+
+public class Cat extends Animal {
+
+    @Override
+    public void makeSound() {
+        System.out.println("Meow");
+    }
+
+    public void scratch(){
+        System.out.println("I'm a cat. I scratch things.");
+    }
+}
